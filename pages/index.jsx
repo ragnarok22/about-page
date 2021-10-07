43174552b2adf4bb01c6f5daaafe854b0e295cbb
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-07 16:27:55 -0400

    feat: agregando la parte visual del inicio

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,55 @@
-import Link from 'next/link'
-import Layout, {siteTitle} from '../components/layouts/base'
-import Footer from '../components/footer'
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout, {siteTitle} from '../components/layouts/base'
+import profilePic from '../public/me.jpg'
+import styles from '../styles/Index.module.scss'
 
 export default function Home() {
+  const profileWidth = 3376 / 10;
+  const profileHeight = 5063 / 10;
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <main>
-        <Link href="/about">
-          <a>About me</a>
-        </Link>
-      </main>
+      <main className={styles.main}>
+        <h1 className="mt-4">Reinier Hernández Avila</h1>
 
-      <Footer/>
+        <div className="flex flex-grow justify-center">
+          <div className="flex flex-col justify-evenly items-center w-1/3">
+            <Link href="/about">
+              <a>Sobre mi</a>
+            </Link>
+            <Link href="/connect">
+              <a className={styles.right}>Conecta</a>
+            </Link>
+            <Link href="/contact">
+              <a>Contacto</a>
+            </Link>
+          </div>
+
+          <Image
+            src={profilePic}
+            alt="Reinier Hernández Avila"
+            width={profileWidth}
+            height={profileHeight}
+            className={`${styles.profile}  w-1/3`}
+          />
+          
+          <div className="flex flex-col w-1/3 justify-evenly items-center">
+            <Link href="/projects">
+              <a>Proyectos</a>
+            </Link>
+            <Link href="/tools">
+              <a className={styles.left}>Herramientas</a>
+            </Link>
+            <Link href="/projects">
+              <a>Proyectos</a>
+            </Link>
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
