3eb2301f4e0a4941eed8f70031c57da0ec69bc6c
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-15 16:46:43 -0400

    feat: mostrando la vista mobile en el home

--- components/Home/Desktop.jsx
+++ components/Home/Desktop.jsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import Layout, {siteTitle} from '../components/layouts/base'
-import profilePic from '../public/me.jpg'
-import styles from '../styles/Mobile.module.scss'
+import Layout, {siteTitle} from '../layouts/base'
+import profilePic from '../../public/me.jpg'
+import styles from '../../styles/Index.module.scss'
 
-export default function Home() {
+export default function HomeDesktop() {
   const profileWidth = 3376 / 10;
   const profileHeight = 5063 / 10;
   return (
@@ -14,9 +14,9 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <main className={styles.main}>
-        <h1>Reinier Hernández Avila</h1>
+        <h1 className="mt-4">Reinier Hernández Avila</h1>
 
-        <div className={styles.content}>
+        <div className="flex flex-grow justify-center">
           <div className={styles.link_list}>
             <Link href="/about">
               <a>Sobre mi</a>
@@ -27,6 +27,17 @@ export default function Home() {
             <Link href="/contact">
               <a>Contacto</a>
             </Link>
+          </div>
+
+          <Image
+            src={profilePic}
+            alt="Reinier Hernández Avila"
+            width={profileWidth}
+            height={profileHeight}
+            className={`${styles.profile}  w-1/3`}
+          />
+          
+          <div className={styles.link_list}>
             <Link href="/projects">
               <a>Proyectos</a>
             </Link>
@@ -37,17 +48,9 @@ export default function Home() {
               <a>Proyectos</a>
             </Link>
           </div>
-
-          <Image
-            src={profilePic}
-            alt="Reinier Hernández Avila"
-            width={profileWidth}
-            height={profileHeight}
-            className={styles.profile}
-          />
         </div>
         <div className={styles.ocean}>
-          <div className={styles.wave} />
+          <div className={styles.wave}></div>
         </div>
       </main>
     </Layout>
---
+++ components/Home/Mobile.jsx
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout, {siteTitle} from '../layouts/base'
+import profilePic from '../../public/me.jpg'
+import styles from '../../styles/Mobile.module.scss'
+
+export default function HomeMobile() {
+  const profileWidth = 3376 / 10;
+  const profileHeight = 5063 / 10;
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <main className={styles.main}>
+        <h1>Reinier Hernández Avila</h1>
+
+        <div className={styles.content}>
+          <div className={styles.link_list}>
+            <Link href="/about">
+              <a>Sobre mi</a>
+            </Link>
+            <Link href="/connect">
+              <a className={styles.right}>Conecta</a>
+            </Link>
+            <Link href="/contact">
+              <a>Contacto</a>
+            </Link>
+            <Link href="/projects">
+              <a>Proyectos</a>
+            </Link>
+            <Link href="/tools">
+              <a className={styles.left}>Herramientas</a>
+            </Link>
+            <Link href="/projects">
+              <a>Proyectos</a>
+            </Link>
+          </div>
+
+          <Image
+            src={profilePic}
+            alt="Reinier Hernández Avila"
+            width={profileWidth}
+            height={profileHeight}
+            className={styles.profile}
+          />
+        </div>
+        <div className={styles.ocean}>
+          <div className={styles.wave} />
+        </div>
+      </main>
+    </Layout>
+  )
+}
