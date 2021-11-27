62be6b81d7a94525bee0da3a78ad7fb4e81f6b2a
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-27 11:06:02 -0500

    chore: add favicons

--- components/layouts/base.jsx
+++ components/layouts/base.jsx
@@ -9,9 +9,12 @@ const BaseLayout = ({ children, className }) => {
   return (
     <div>
       <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <title>{siteTitle}</title>
         <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={className}>
         {children}
