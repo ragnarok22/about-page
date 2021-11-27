bc7bfc773e5118c896383f9fac28fdd72dc3e56a
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-27 09:11:34 -0500

    style: not found with lottie files

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,5 +1,21 @@
+import Link from 'next/link'
+import { Player } from "@lottiefiles/react-lottie-player"
+
+import styles from "../styles/NotFound.module.scss"
+import lottieNotFound from "../assets/lottie/404.json"
+
 export default function Custom404() {
   return (
-    <h1>Not found</h1>
+    <div className={styles.not_found}>
+      <Player
+        autoplay
+        src={lottieNotFound}
+        className={styles.lottie}
+      >
+      </Player>
+      <Link href="/">
+        <a className="underline text-green-900">Inicio</a>
+      </Link>
+    </div>
   )
 }
