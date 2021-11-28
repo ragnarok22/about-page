2d85dff6278fd3e0652ef50268f68d67f0201aef
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-28 10:32:51 -0500

    fix: link and icon position

--- components/connect/ButtonLink.jsx
+++ components/connect/ButtonLink.jsx
@@ -1,8 +1,9 @@
 const ButtonLink = ({ icon, link, text }) => {
   return (
-    <div className="flex bg-gray-100 text-gray-900 p-2 rounded mb-2">
-      <a href={link} className="" target="_blank" rel="noopener noreferrer">
-        {icon} {text}
+    <div className="flex bg-gray-100 text-gray-900 p-2 rounded mb-2 relative">
+      <a href={link} className="w-full text-center" target="_blank" rel="noopener noreferrer">
+        <div className="absolute">{icon}</div>
+        {text}
       </a>
     </div>
   );
--- data/content.js
+++ data/content.js
@@ -2,11 +2,11 @@ export const content = [
   {
     text: "Mi blog personal",
     icon: "blog",
-    url: "https://blog.ragnarok22.dev",
+    link: "https://blog.ragnarok22.dev",
   },
   {
     text: "Mi boletín",
     icon: "newsletter",
-    url: "https://newsletter.ragnarok22.dev",
+    link: "https://newsletter.ragnarok22.dev",
   },
 ];
