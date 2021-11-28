2134fd2d219ef90ed789f5266193c0401e5b380d
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-28 09:42:57 -0500

    feat: send emails

--- components/layouts/base.jsx
+++ components/layouts/base.jsx
@@ -9,7 +9,7 @@ const BaseLayout = ({ children, className }) => {
   return (
     <div>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,9 +1,11 @@
+import ContactForm from '../components/contact/ContactForm';
 import PageLayout from '../components/layouts/page';
 
 const ContactPage = () => {
   return (
-    <PageLayout>
+    <PageLayout className="flex flex-col items-center">
       <h1 className="page-title">Contacto</h1>
+      <ContactForm />
     </PageLayout>
   )
 }
